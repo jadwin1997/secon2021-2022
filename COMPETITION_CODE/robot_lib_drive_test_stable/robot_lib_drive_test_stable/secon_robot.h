e6b969da76692817b804c5aff962adc1903ac051
jadwin1997 <jadwinmanuel@gmail.com>
2022-03-10 15:08:46 -0600

    Add files via upload

--- COMPETITION_CODE/robot_lib_drive_test_stable/robot_lib_drive_test_stable/secon_robot.h
+++ COMPETITION_CODE/robot_lib_drive_test_stable/robot_lib_drive_test_stable/secon_robot.h
@@ -9,41 +9,41 @@
 #include <Servo.h>
 #include "TFMini.h"
 #include <SoftwareSerial.h>
-
+#include <MPU6050.h>
 //TFMini tfmini1;
 //TFMini tfmini2;
 
 #define SERVOMIN_BELT 100  // This is the 'minimum' pulse length count (out of 4096)
 #define SERVOMAX_BELT  500 // This is the 'maximum' pulse length count (out of 4096)
-/*
-const int stepsPerRevolution = 200;
-Servo belt;
-Servo shooter;
-Servo arm;
-Servo cam;
 
-Stepper myStepper(stepsPerRevolution,3,4,5,6);
-float angle2 = 0.0;
-SoftwareSerial rearLidar(6,5);
-SoftwareSerial portLidar(7,8);
-// Create the motor shield object with the default I2C address
-Adafruit_MotorShield AFMS = Adafruit_MotorShield(0x60); 
-Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
-// Or, create it with a different I2C address (say for stacking)
-// Adafruit_MotorShield AFMS = Adafruit_MotorShield(0x61); 
-
-// Select which 'port' M1, M2, M3 or M4. In this case, M1
-Adafruit_DCMotor *motor1 = AFMS.getMotor(1);
-Adafruit_DCMotor *motor2 = AFMS.getMotor(2);
-Adafruit_DCMotor *motor3 = AFMS.getMotor(3);
-Adafruit_DCMotor *motor4 = AFMS.getMotor(4);
-*/
 class robot
 {
+  
   public:
     robot();
     void anglemove(float angle, int velocity);
+    void calibrateSensors();
+    int leftDistance();
+    void updateGyro();
+    void updateAngle();
+    int serialTalk();
+    int getZaccel();
+    int16_t ax, ay, az;
+    int16_t gx, gy, gz;
+    unsigned long timeOld;
+    void setCoordinates(int x, int y);
+    void moveToCoordinates(int mode);
+    void updateY();
+    int x_coordinate;
+    int y_coordinate;
+    //int target_x;
+    //int target_y;
+    float angle;
     //const int stepsPerRevolution = 200;
+    MPU6050 mpu;
+    TFMini tfmini1;
+    int p;
+    //TFMini tfmini2;
     Servo belt;
     Servo shooter;
     Servo arm;
@@ -54,14 +54,19 @@ class robot
     Adafruit_DCMotor *motor2;
     Adafruit_DCMotor *motor3;
     Adafruit_DCMotor *motor4;
+    SoftwareSerial portLidar{7, 8};
+    int getZaccelOffset();
+    int offset;
     //Stepper myStepper(stepsPerRevolution,3,4,5,6);
     //float angle2 = 0.0;
-    //SoftwareSerial rearLidar(6,5);
-    //SoftwareSerial portLidar();
+    
+    
   
     
   private:
   float maptovel(int in,int velocity);
+  int target_x;
+  int target_y;
 
     
 };
