7e6db734f80a729ce29865a0065b4ba525c42221
jadwin1997 <jadwinmanuel@gmail.com>
2022-03-10 15:07:52 -0600

    Add files via upload

--- COMPETITION_CODE/robot_lib_drive_test_stable/robot_lib_drive_test_stable/robot_lib_drive_test_stable.ino
+++ COMPETITION_CODE/robot_lib_drive_test_stable/robot_lib_drive_test_stable/robot_lib_drive_test_stable.ino
@@ -4,12 +4,23 @@ void setup() {
   Serial.begin(9600);
   // put your setup code here, to run once:
   robo.AFMS.begin();
+  robo.calibrateSensors();
+  robo.setCoordinates(100,100);
+  //robo.portLidar.begin(TFMINI_BAUDRATE);
 }
 
 void loop() {
   
   // put your main code here, to run repeatedly:
+  Serial.print("angle: ");
+  Serial.print(robo.angle);
+  Serial.print(" y: ");
+  Serial.println(robo.y_coordinate);
   
-  robo.anglemove(45.0,100);
-  delay(100);
+  
+  robo.moveToCoordinates(1);
+  //robo.anglemove(90.0,100);
+  robo.updateGyro();
+  robo.updateAngle();
+  robo.updateY();
 }
